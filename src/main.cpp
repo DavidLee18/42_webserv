558c24f8b217e345d8c9620403b24d059c8650b1
DavidLee18 <djwodus@gmail.com>
2025-11-09 02:07:11 +0900

    add the basic wrapper classes

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,23 @@
 //
 // Created by 이재현 on 2025-11-08.
 //
-#include <iostream>
+#include "webserv.h"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main(const int argc, char *argv[]) {
+    if (argc != 2) {
+        std::cerr << "Usage: webserv <config_file>" << std::endl;
+        return 1;
+    }
+    (void)argv;
+    #ifdef __APPLE__
+    #else
+    EPoll e_poll;
+    #endif
+    signal(SIGINT, wrap_up);
+    while (sig == 0) {
+        std::cout << "Hello, World!" << std::endl;
+    }
     return 0;
 }
+
+void wrap_up(const int signum) { sig = signum; }
--- src/webserv.h
+++ src/webserv.h
@@ -6,11 +6,14 @@
 #define WEBSERV_H
 #include <arpa/inet.h>
 #include <cerrno>
+#include <csignal>
 #include <cstring>
 #include <dirent.h>
 #include <fcntl.h>
+#include <iostream>
+#include <iterator>
 #include <netdb.h>
-#include <csignal>
+#include <string>
 #ifdef __APPLE__
 #include <sys/event.h>
 #include <sys/time.h>
@@ -22,4 +25,54 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
-#endif //WEBSERV_H
+
+volatile sig_atomic_t sig = 0;
+
+void wrap_up(int);
+
+class FileDescriptor {
+    int _fd;
+    public:
+        explicit FileDescriptor(int);
+        FileDescriptor move_from(FileDescriptor&);
+        ~FileDescriptor();
+        const int& operator*() const;
+        void set_blocking(bool);
+};
+
+#ifdef __APPLE__
+#else
+/**
+ * @class EPoll
+ * @brief A simple epoll wrapper class.
+ */
+class EPoll {
+    /**
+     * @class Events
+     * @brief A read-only iterator of epoll events.
+     */
+    class Events : public std::iterator<std::input_iterator_tag, struct epoll_event, long, const
+                    epoll_event*, const epoll_event&> {
+            size_t _curr;
+            epoll_event *_Nonnull _events;
+            size_t _len;
+            public:
+                explicit Events(size_t);
+                ~Events();
+                Events& operator++();
+                Events operator++(int);
+                bool operator==(const Events&) const;
+                bool operator!=(const Events&) const;
+                const epoll_event& operator*() const;
+    };
+    public:
+        typedef Events Events;
+        EPoll();
+        ~EPoll();
+        void add_fd(const FileDescriptor& fd, epoll_event *_Nonnull event);
+        void modify_fd(const FileDescriptor& fd, epoll_event *_Nonnull event);
+        void del_fd(const FileDescriptor& fd, epoll_event *_Nonnull event);
+        Events& wait(uint16_t timeout_ms);
+};
+#endif
+#endif
