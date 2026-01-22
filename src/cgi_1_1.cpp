c27b44c7c9431272f5b7346365190b23b917f80d
Jaehyun Lee <jaehylee@student.42gyeongsan.kr>
2026-01-23 03:32:16 +0900

    :construction: building parsers..

--- src/cgi_1_1.cpp
+++ src/cgi_1_1.cpp
@@ -8,46 +8,107 @@ Result<Void> ContentType::add_param(std::string k, std::string v) {
   return OKV;
 }
 
-ServerName *ServerName::host(std::string raw) {
-  std::stringstream ss(raw);
-  std::list<std::string> *hostparts = new std::list<std::string>();
-  std::string part;
-  std::getline(ss, part, '.');
-  while (ss && !ss.eof()) {
-    hostparts->push_back(part);
-    std::getline(ss, part, '.');
-  }
-  return new ServerName(Host, (ServerNameVal){.host_name = hostparts});
+ServerName *ServerName::host(std::list<std::string> hostparts) {
+  return new ServerName(
+      Host,
+      (ServerNameVal){.host_name = new std::list<std::string>(hostparts)});
 }
 
 ServerName *ServerName::ipv4(unsigned char b1, unsigned char b2,
                              unsigned char b3, unsigned char b4) {
   return new ServerName(Ipv4, (ServerNameVal){.ipv4 = {b1, b2, b3, b4}});
 }
-
-Result<ServerName *> ServerName::ipv4(std::string raw) {
+Result<std::pair<ServerName *, size_t> >
+ServerName::Parser::parse_host(std::string raw) {
   std::stringstream ss(raw);
-  std::vector<std::string> *ips = new std::vector<std::string>();
+  std::list<std::string> parts;
   std::string part;
+  bool dom_end = false;
+  size_t j = 0;
   std::getline(ss, part, '.');
   while (ss && !ss.eof()) {
+    if ((!dom_end && !std::isalnum(part[0])) || !std::isalpha(part[0]))
+      return ERR_PAIR(ServerName *, size_t, Errors::invalid_format);
+    j++;
+    for (size_t i = 1; i < part.size() - 1; i++) {
+      if (!std::isalnum(part[i]) && part[i] != '-')
+        return ERR_PAIR(ServerName *, size_t, Errors::invalid_format);
+    }
+    if (!std::isalnum(part[part.size() - 1]))
+      return ERR_PAIR(ServerName *, size_t, Errors::invalid_format);
+    j += part.size();
+    std::getline(ss, part, '.');
+  }
+  if (!ss.eof())
+    return ERR_PAIR(ServerName *, size_t, Errors::invalid_format);
+  if ((!dom_end && !std::isalnum(part[0])) || !std::isalpha(part[0]))
+    return ERR_PAIR(ServerName *, size_t, Errors::invalid_format);
+  j++;
+  for (size_t i = 1; i < part.size() - 1; i++) {
+    if (!std::isalnum(part[i]) && part[i] != '-')
+      return ERR_PAIR(ServerName *, size_t, Errors::invalid_format);
+  }
+  if (!std::isalnum(part[part.size() - 1]))
+    return ERR_PAIR(ServerName *, size_t, Errors::invalid_format);
+  return OK_PAIR(ServerName *, size_t, ServerName::host(parts),
+                 j + part.size());
+}
+Result<std::pair<ServerName *, size_t> >
+ServerName::Parser::parse_ipv4(std::string raw) {
+  std::stringstream ss(raw);
+  std::vector<std::string> *ips = new std::vector<std::string>();
+  std::vector<unsigned char> addrs;
+  std::string part;
+  size_t i = 0;
+  std::getline(ss, part, '.');
+  while (ss && !ss.eof()) {
+    if (part.size() < 1 || part.size() > 3)
+      return ERR_PAIR(ServerName *, size_t, Errors::invalid_format);
+    for (size_t j = 0; j < part.size(); j++) {
+      if ('0' < part[j] || part[j] > '9')
+        return ERR_PAIR(ServerName *, size_t, Errors::invalid_format);
+    }
+    i += part.size();
+    addrs.push_back(static_cast<unsigned char>(std::atoi(part.c_str())));
     ips->push_back(part);
     std::getline(ss, part, '.');
   }
-  if (ips->size() != 4)
-    return ERR(ServerName *, Errors::invalid_format);
-  unsigned char addrs[4];
-  for (size_t i = 0; i < 4; i++) {
-    if ((*ips)[i].size() < 1 || (*ips)[i].size() > 3)
-      return ERR(ServerName *, Errors::invalid_format);
-    for (size_t j = 0; j < (*ips)[i].size(); j++) {
-      if ('0' < (*ips)[i][j] || (*ips)[i][j] > '9')
-        return ERR(ServerName *, Errors::invalid_format);
-    }
-    addrs[i] = static_cast<unsigned char>(std::atoi((*ips)[i].c_str()));
+  if (!ss.eof())
+    return ERR_PAIR(ServerName *, size_t, Errors::invalid_format);
+  if (part.size() < 1 || part.size() > 3)
+    return ERR_PAIR(ServerName *, size_t, Errors::invalid_format);
+  for (size_t j = 0; j < part.size(); j++) {
+    if ('0' < part[j] || part[j] > '9')
+      return ERR_PAIR(ServerName *, size_t, Errors::invalid_format);
   }
-  ServerName **res = new ServerName *;
-  *res = new ServerName(
-      Ipv4, (ServerNameVal){.ipv4 = {addrs[0], addrs[1], addrs[2], addrs[3]}});
-  return OK(ServerName *, res);
+  addrs.push_back(static_cast<unsigned char>(std::atoi(part.c_str())));
+  ips->push_back(part);
+  if (addrs.size() != 4)
+    return ERR_PAIR(ServerName *, size_t, Errors::invalid_format);
+  return OK_PAIR(ServerName *, size_t,
+                 ServerName::ipv4(addrs[0], addrs[1], addrs[2], addrs[3]),
+                 i + part.size());
 }
+
+Result<std::pair<ServerName *, size_t> >
+ServerName::Parser::parse(std::string raw) {}
+
+// CgiMetaVar *CgiMetaVar::auth_type(CgiAuthType ty) {
+//   return new CgiMetaVar(AUTH_TYPE, (CgiMetaVar_){.auth_type = ty});
+// }
+
+// CgiMetaVar *CgiMetaVar::content_length(unsigned int l) {
+//   return new CgiMetaVar(CONTENT_LENGTH, (CgiMetaVar_){.content_length = l});
+// }
+
+// CgiMetaVar *CgiMetaVar::content_type(ContentType ty) {
+//   return new CgiMetaVar(CONTENT_TYPE,
+//                         (CgiMetaVar_){.content_type = new ContentType(ty)});
+// }
+
+// CgiMetaVar *CgiMetaVar::gateway_interface(GatewayInterface i) {
+//   return new CgiMetaVar(GATEWAY_INTERFACE,
+//                         (CgiMetaVar_){.gateway_interface = i});
+// }
+
+// Result<CgiMetaVar *> CgiMetaVar::path_info(std::string raw) {}
--- src/cgi_1_1.h
+++ src/cgi_1_1.h
@@ -43,6 +43,8 @@ class ContentType {
 public:
   ContentType(std::string ty, std::string subty)
       : type(ty), subtype(subty), params() {}
+  ContentType(ContentType const &other)
+      : type(other.type), subtype(other.subtype), params(other.params) {}
   Result<Void> add_param(std::string, std::string);
 };
 
@@ -60,10 +62,19 @@ class ServerName {
 
   ServerName(ServerNameType ty, ServerNameVal v) : type(ty), val(v) {}
 
+  static ServerName *host(std::list<std::string>);
+  static ServerName *ipv4(unsigned char, unsigned char, unsigned char,
+                          unsigned char);
+
 public:
-  ServerName *host(std::string);
-  ServerName *ipv4(unsigned char, unsigned char, unsigned char, unsigned char);
-  Result<ServerName *> ipv4(std::string);
+  class Parser {
+    virtual void phantom() = 0;
+    static Result<std::pair<ServerName *, size_t> > parse_host(std::string);
+    static Result<std::pair<ServerName *, size_t> > parse_ipv4(std::string);
+
+  public:
+    static Result<std::pair<ServerName *, size_t> > parse(std::string);
+  };
 };
 
 enum ServerProtocol { Http_1_1 };
@@ -72,32 +83,54 @@ enum ServerSoftware { Webserv };
 
 enum EtcMetaVarType { Http, Custom };
 
+union CgiMetaVar_ {
+  CgiAuthType auth_type;
+  unsigned int content_length;
+  ContentType *content_type;
+  GatewayInterface gateway_interface;
+  std::list<std::string> *path_info;
+  std::string *path_translated;
+  std::map<std::string, std::string> *query_string;
+  unsigned char remote_addr[4];
+  std::list<std::string> *remote_host;
+  std::string *remote_ident;
+  std::string *remote_user;
+  HttpMethod request_method;
+  std::list<std::string> *script_name;
+  ServerName *server_name;
+  unsigned short server_port;
+  ServerProtocol server_protocol;
+  ServerSoftware server_software;
+  struct EtcMetaVar {
+    EtcMetaVarType type;
+    std::string name;
+    std::string value;
+  } *etc_val;
+};
 class CgiMetaVar {
   CgiMetaVarName name;
-  union CgiMetaVar_ {
-    CgiAuthType auth_type;
-    unsigned int content_length;
-    ContentType *content_type;
-    GatewayInterface gateway_interface;
-    std::list<std::string> *path_info;
-    std::string *path_translated;
-    std::map<std::string, std::string> *query_string;
-    unsigned char remote_addr[4];
-    std::list<std::string> *remote_host;
-    std::string *remote_ident;
-    std::string *remote_user;
-    HttpMethod request_method;
-    std::list<std::string> *script_name;
-    ServerName *server_name;
-    unsigned short server_port;
-    ServerProtocol server_protocol;
-    ServerSoftware server_software;
-    struct EtcMetaVar {
-      EtcMetaVarType type;
-      std::string name;
-      std::string value;
-    } *etc_val;
-  } val;
+  CgiMetaVar_ val;
+  CgiMetaVar(CgiMetaVarName n, CgiMetaVar_ v) : name(n), val(v) {}
+
+public:
+  static CgiMetaVar *auth_type(CgiAuthType);
+  static CgiMetaVar *content_length(unsigned int);
+  static CgiMetaVar *content_type(ContentType);
+  static CgiMetaVar *gateway_interface(GatewayInterface);
+  static Result<CgiMetaVar *> path_info(std::string);
+  static CgiMetaVar *path_translated(std::string);
+  static Result<CgiMetaVar *> query_string(std::string);
+  static Result<CgiMetaVar *> remote_addr(std::string);
+  static CgiMetaVar *remote_host(std::string);
+  static CgiMetaVar *remote_ident(std::string);
+  static CgiMetaVar *remote_user(std::string);
+  static CgiMetaVar *request_method(HttpMethod);
+  static CgiMetaVar *script_name(std::string);
+  static CgiMetaVar *server_name(ServerName *);
+  static CgiMetaVar *server_port(unsigned short);
+  static CgiMetaVar *server_protocol(ServerProtocol);
+  static CgiMetaVar *server_software(ServerSoftware);
+  static CgiMetaVar *custom_var(EtcMetaVarType, std::string, std::string);
 };
 
 class CgiInput {
