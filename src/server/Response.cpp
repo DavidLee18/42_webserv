1c68b2fec753144ee9136309785a64fc69e84210
Jaehyun Lee <jaehylee@student.42gyeongsan.kr>
2026-05-11 21:45:40 +0900

    :bug: fix a bug in path-traversal defence

--- src/server/Response.cpp
+++ src/server/Response.cpp
@@ -119,7 +119,7 @@ Response ServerResponse::http_response(
     response = DefaultError::default_err_response(Response::NOT_FOUND);
     response.headers = config->get_header();
     return response;
-  } else if (request->get_path().find("../")) {
+  } else if (request->get_path().find("../") != std::string::npos) {
     std::cout << "request path: " << request->get_path() << std::endl;
     response = DefaultError::default_err_response(Response::BAD_REQUEST);
     response.headers = config->get_header();
