566d6aca2c17eda6ef3b343b8be395805fe14a86
Jaehyun Lee <jaehylee@student.42gyeongsan.kr>
2026-05-15 00:31:55 +0900

    :adhesive_bandage: a small adjustment to the test suite

--- tests/run_all.zsh
+++ tests/run_all.zsh
@@ -15,7 +15,7 @@ PYTHON=${PYTHON:-$(command -v python3)}
 
 # Sensible defaults for suite-specific env vars.
 export HOST PORT PYTHON
-export UPLOAD_URL=${UPLOAD_URL:-/storage/}
+export UPLOAD_URL=${UPLOAD_URL:-/cgi-test/echo}
 export ECHO_CGI_URL=${ECHO_CGI_URL:-/cgi-test/echo}
 export UPLOAD_PATH=${UPLOAD_PATH:-/www-file/storage/}
 export CGI_EXTRA_ENV_KEYS=${CGI_EXTRA_ENV_KEYS:-LC_CTYPE}
